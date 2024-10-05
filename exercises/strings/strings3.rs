7e2ae1a3547189bd70d5520d46c5301179f06c07
Chris-Smile7 <Chris-Chirs@outlook.com>
2024-10-05 17:31:17 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,17 +7,19 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let trim=str.trim();
+    return trim;
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    s.insert_str(" world!");
+    return s;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    
 }
 
 #[cfg(test)]
